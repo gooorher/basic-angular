e4952185d172126d6f5e264864700ac624ca190d
gooorher <juaaanlu@gmail.com>
2024-09-20 14:02:28 +0200

    20/09/2024

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
 
+const random_index = Math.floor(Math.random() * DUMMY_USERS.length);
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -7,6 +9,22 @@ import { Component } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
+
 export class UserComponent {
+  selectedUser = DUMMY_USERS[random_index];
+
+  get imagePath() {
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
+
+
+  onSelectUser() {
+    console.log('Clicked!');
+    //Generamos otro número aleatorio para cambiar si o sí de usuario cuando hacemos click 
+    const random_index = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[random_index]
+    //this.selectedUser.set(DUMMY_USERS); ¿Por qué no funciona?
+  }
 
 }
+
